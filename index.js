e20a48df4ddf75298cf9e686096e2aac3ea5b4f5
Tukkos <jpfcastro@id.uff.br>
2022-06-23 00:52:33 -0300

    Abrir e fechar menu lateral pelo js e suas configuraçoes em html e css

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+let mensagens;
 
+buscarMensagens();
 
-
-
-
+function buscarMensagens() {
+    const promessa = axios.get()
+}
 
 
 
 function abrirMenuLateral() {
-    const overlay = document.querySelector(`.overlay`);
+    const overlay = document.querySelector(`.menuLateralFundos`).parentNode;
     overlay.classList.toggle(`escondido`)
 }
